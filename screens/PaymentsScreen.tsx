273b0d0be3f66b0586c1027ccfe107d5755ea373
callmebart <bsosniak@gmail.com>
2022-04-18 01:57:55 +0200

    Payment Screen / metamask / ui for transactions / recent transactions

--- screens/PaymentsScreen.tsx
+++ screens/PaymentsScreen.tsx
@@ -30,7 +30,18 @@ export default function PaymentsScreen() {
     const navigation = useNavigation()
     const { theme, setTheme } = useTheme()
 
+    const [textInputColor, setTextInputColor] = useState(Colors.light.textInputColor)
+    useEffect(() => {
+        if (theme == 'light') {
+            setTextInputColor(Colors.light.textInputColor)
+        } else {
+            setTextInputColor(Colors.dark.backgroundColor)
+        }
+    }, [theme])
+
     const dispatch = useDispatch()
+
+    const [scrollEnabled, setScrollEnabled] = useState(true)
 
     const users = useSelector(selectAllUsers)
     const connector = useWalletConnect()
@@ -38,6 +49,7 @@ export default function PaymentsScreen() {
     const [metamaskConnected, setMetamaskConnected] = useState(false)
 
     const connectWallet = React.useCallback(() => {
+        setScrollEnabled(true)
         return connector.connect();
     }, [connector]);
 
@@ -51,9 +63,6 @@ export default function PaymentsScreen() {
             address.length
         )}`;
     }
-
-
-
 
     const renderUsers: ListRenderItem<any> = ({ item, index }) => {
         return (
@@ -82,8 +91,60 @@ export default function PaymentsScreen() {
         )
     }
 
+    const trs = [
+        {
+            id: '#asdfasaskiw92',
+            icon: 'spotify',
+            name: 'Spotify',
+            type: 'Entertaiment',
+            price: '5.00'
+        },
+        {
+            id: '#asd2314231rerfa2',
+            icon: 'shopify',
+            name: 'Shopify',
+            type: 'Food',
+            price: '25.00'
+        },
+        {
+            id: '#asd23142dsfdsfa2',
+            icon: 'hamburger',
+            name: 'Uncles Burgers',
+            type: 'Food',
+            price: '10.00'
+        },
+        {
+            id: '#asd2ggggsfasddsfa2',
+            icon: 'football-ball',
+            name: 'Fitness sharks',
+            type: 'Sport',
+            price: '10.00'
+        }
+    ]
+    const renderTransactions: any = trs.map((item: any) => {
+        console.log(item.id)
+        return (
+            <View key={item.id} style={{ width: windowWidth - 40, padding: 10, flexDirection: 'row', justifyContent: 'space-evenly' }}>
+
+                <View style={{ width: 50, height: 50, elevation: 1, backgroundColor: textInputColor, borderRadius: 15, justifyContent: 'center', alignItems: 'center' }}>
+                    <FontAwesome5 name={item.icon} size={24} color="#9f9f9f" />
+                </View>
+                <View style={{ flexDirection: "row", width: windowWidth / 1.5, height: 50, elevation: 1, backgroundColor: textInputColor, borderRadius: 15, justifyContent: 'space-between', alignItems: 'center' }}>
+                    <View style={{ marginLeft: 15 }}>
+                        <Text style={{ color: '#9f9f9f', fontWeight: 'bold', fontSize: 16 }}>{item.name}</Text>
+                        <Text style={{ color: '#9f9f9f', fontSize: 12 }}>{item.type}</Text>
+                    </View>
+                    <View style={{ marginRight: 15 }}>
+                        <Text style={{ color: '#9f9f9f', fontWeight: 'bold', fontSize: 16, }}>- {item.price}$</Text>
+                    </View>
+                </View>
+
+            </View>
+        )
+    })
+
     return (
-        <ScrollView style={[themeMode[theme]]} showsVerticalScrollIndicator={false}>
+        <ScrollView style={[themeMode[theme]]} showsVerticalScrollIndicator={false} scrollEnabled={scrollEnabled}>
             <View style={styles.header}>
                 <NeoumorphicBox>
                     <View style={{ width: windowWidth - 40, height: 70, borderRadius: 20, justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center' }}>
@@ -136,6 +197,28 @@ export default function PaymentsScreen() {
                                 </View>
 
                             </View>
+                            <View style={{ justifyContent: 'center', marginLeft: 20, marginTop: 65 }}>
+                                <NeoumorphicBox>
+                                    <View style={{ width: windowWidth - 40, height: 180, borderRadius: 20, }}>
+                                        <View >
+                                            <Text style={styles.h3Title}>Your address: {shortenAddress(connector.accounts[0])}</Text>
+                                        </View>
+                                        <View>
+                                            <Text style={styles.h3Title}>Select asset</Text>
+                                            <View style={{ ...styles.transStyle, backgroundColor: textInputColor, justifyContent: 'space-between', flexDirection: 'row' }}>
+                                                <Text style={{ color: Colors.headerTextColor, marginTop: 5 }}>Ethereum</Text>
+                                                <MaterialIcons name="keyboard-arrow-down" size={30} color={Colors.headerTextColor} style={{ marginRight: 10 }} />
+                                            </View>
+                                        </View>
+                                        <View>
+                                            <Text style={styles.h3Title}>Type in amount</Text>
+                                            <View style={{ ...styles.transStyle, backgroundColor: textInputColor, alignItems: 'flex-start', justifyContent: 'center' }}>
+                                                <Text style={{ color: Colors.headerTextColor }}>0.25 eth</Text>
+                                            </View>
+                                        </View>
+                                    </View>
+                                </NeoumorphicBox>
+                            </View>
 
                             <View style={{ marginLeft: 20, marginTop: 60 }}>
                                 <NeoumorphicBox>
@@ -150,7 +233,7 @@ export default function PaymentsScreen() {
                             <View style={{ marginLeft: 20, marginTop: 60, flexDirection: 'row', width: windowWidth }}>
 
                                 <NeoumorphicBox>
-                                    <TouchableOpacity onPress={() => killSession()} style={{ width:windowWidth-40, height: 50, borderRadius: 20, justifyContent: 'center', alignItems: 'center' }}>
+                                    <TouchableOpacity onPress={() => killSession()} style={{ width: windowWidth - 40, height: 50, borderRadius: 20, justifyContent: 'center', alignItems: 'center' }}>
                                         <View style={{ flexDirection: 'row', opacity: 0.6, width: windowWidth / 2.5, justifyContent: 'space-evenly' }}>
                                             <FontAwesome5 name="book-dead" size={20} color="#ff8800" />
                                             <Text style={{ color: '#9f9f9f', fontSize: 13 }}>KILL SESSION</Text>
@@ -160,6 +243,8 @@ export default function PaymentsScreen() {
 
 
                             </View>
+
+
 
                         </View>
                         :
@@ -173,21 +258,23 @@ export default function PaymentsScreen() {
 
                     }
                 </View>
+                <View style={{ marginTop: 60 }}>
+                    <Text style={styles.h2Title}>Recent Transactions</Text>
+                    <View style={{ justifyContent: 'center', marginLeft: 20, marginTop: 20 }}>
 
-                <View style={{ justifyContent: 'center', marginLeft: 20, marginTop: 65 }}>
-                    <NeoumorphicBox>
-                        <ScrollView style={{ width: windowWidth - 40, height: 230, borderRadius: 20, }}>
-                            {connector.connected ?
-                                <Text>
-                                    {shortenAddress(connector.accounts[0])}
-                                </Text>
-                                : <Text></Text>
-                            }
-                        </ScrollView>
-                    </NeoumorphicBox>
+                        <NeoumorphicBox>
+                            <ScrollView style={{ width: windowWidth - 40, height: 230, borderRadius: 20, }}
+                                onTouchStart={(ev)=>setScrollEnabled(false)}
+                                onMomentumScrollEnd={(e)=>setScrollEnabled(true)}
+                                onScrollEndDrag={(e)=>setScrollEnabled(true)}
+                            >
+                                {renderTransactions}
+                            </ScrollView>
+                        </NeoumorphicBox>
+                    </View>
                 </View>
             </View>
-            <View style={{ width: windowWidth - 40, height: 100 }}></View>
+            <View style={{ width: windowWidth - 40, height: 50,marginTop:10 }}></View>
         </ScrollView>
     )
 }
@@ -220,10 +307,27 @@ const styles = StyleSheet.create({
         paddingHorizontal: 20,
         paddingTop: 10,
     },
+    h3Title: {
+        color: Colors.basicTextGrey,
+        fontSize: 13,
+        paddingHorizontal: 20,
+        paddingTop: 10,
+    },
     textInput: {
         height: 40,
         width: 150,
         textAlign: 'center',
         color: Colors.basicTextGrey,
+    },
+    transStyle: {
+        width: windowWidth - 60,
+        height: 30,
+        marginTop: 8,
+        borderRadius: 7,
+        alignSelf: 'center',
+        paddingLeft: 10,
+        color: Colors.basicTextGrey,
+        opacity: 1,
+        elevation: 1,
     },
 });
